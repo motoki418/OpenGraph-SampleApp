7be3e49f0a034d29235ff145221393199c91fb42
motoki418 <snkrs418@gmail.com>
2022-03-14 21:36:10 +0900

    Initial Commit

---
+++ OpenGraph-Sample/OpenGraph_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenGraph_SampleApp.swift
+//  OpenGraph-Sample
+//
+//  Created by nakamura motoki on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct OpenGraph_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
